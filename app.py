f36f0c90f3cdea9d9f7ae4e32854319c57bc1030
askvart <askvart@gmail.com>
2019-09-20 19:17:41 +0300

    no-end commit

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
+import feedparser
 from flask import Flask
 from flask import render_template
-import feedparser
+from flask import request
+
+
 
 app = Flask(__name__)
 
+# Ленты RSS из интернета
 RSS_FEEDS = {'avto': 'https://news.yandex.ru/auto.rss',
              'nedviga':'https://news.yandex.ru/realty.rss',
              'theatr': 'https://news.yandex.ru/theaters.rss',
              'lenta':'https://lenta.ru/rss/top7',
-             'fin': 'https://news.yandex.ru/finances.rss'}
+             'fin': 'http://www.finmarket.ru/rss/mainnews.asp',
+             'vc':'http://siliconrus.com/feed/',
+             'xbt': 'http://ixbt.com/export/news.rss'}
 
 
 @app.route('/')
 @app.route('/<publication>')
 def get_news(publication='lenta'):
+    ''' Функция парсит rss и рендерит шаблон'''
+    query = request.args.get('publication')
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = 'lenta'
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template('home.html', articles=feed['entries'])
+    return render_template('home.html',
+                           articles=feed['entries'])
 
 
 
